e74d92840ef2786d18618f6884eb50da57e10a61
eskarpnes <erl1sharpy@gmail.com>
2018-10-11 23:45:41 +0200

    Unit test for save/load and counter logic

--- ConsoleTodoTest/TodoUnitTest.cs
+++ ConsoleTodoTest/TodoUnitTest.cs
@@ -44,7 +44,7 @@ namespace ConsoleTodoTest
             using (var output = new ConsoleOutput())
             {
                 todo.AddTodo("Make unit test");
-                Assert.AreEqual(expected.Trim(), output.GetOutput());
+                Assert.AreEqual(expected, output.GetOutput());
             }
             Assert.AreEqual(currentConsoleOut, Console.Out);
         }
@@ -118,5 +118,82 @@ namespace ConsoleTodoTest
             Assert.AreEqual(currentConsoleOut, Console.Out);
         }
 
+        [TestMethod]
+        public void TestDoubleDigitIdPrintSort()
+        {
+            Todo todo = new Todo();
+            todo.Initialize("Erlend");
+
+            for (int i = 0; i <= 10; i++)
+            {
+                todo.AddTodo("x");
+            }
+
+            string expected = "#1 x\r\n#2 x\r\n#3 x\r\n#4 x\r\n#5 x\r\n#6 x\r\n#7 x\r\n#8 x\r\n#9 x\r\n#10 x\r\n#11 x";
+
+            var currentConsoleOut = Console.Out;
+            using (var output = new ConsoleOutput())
+            {
+                todo.PrintTodo();
+                Assert.AreEqual(expected, output.GetOutput());
+            }
+            Assert.AreEqual(currentConsoleOut, Console.Out);
+        }
+
+        [TestMethod]
+        public void TestIdCounter()
+        {
+            Todo todo = new Todo();
+            todo.Initialize("Erlend");
+            todo.AddTodo("1");
+            todo.AddTodo("2");
+            todo.RemoveTodo("#1");
+            todo.AddTodo("3");
+
+            string expected = "#2 2\r\n#3 3";
+
+            var currentConsoleOut = Console.Out;
+            using (var output = new ConsoleOutput())
+            {
+                todo.PrintTodo();
+                Assert.AreEqual(expected, output.GetOutput());
+            }
+            Assert.AreEqual(currentConsoleOut, Console.Out);
+
+        }
+
+        [TestMethod]
+        public void TestSaveTodo()
+        {
+            Todo todo = new Todo();
+            todo.Initialize("Erlend");
+
+            Program.WriteToBinaryFile<Todo>("testpath", todo);
+
+            Assert.IsTrue(File.Exists("testpath"));
+
+            File.Delete("testpath");
+
+            Assert.IsFalse(File.Exists("testpath"));
+        }
+
+        [TestMethod]
+        public void TestLoadTodo()
+        {
+            Todo todo = new Todo();
+            todo.Initialize("Erlend");
+
+            Program.WriteToBinaryFile<Todo>("testpath", todo);
+
+            Todo loadedTodo = Program.ReadFromBinaryFile<Todo>("testpath");
+
+            Assert.IsNotNull(loadedTodo);
+            Assert.IsInstanceOfType(loadedTodo, typeof(Todo));
+            Assert.AreEqual("Erlend", loadedTodo.Author);
+
+            File.Delete("testpath");
+
+            Assert.IsFalse(File.Exists("testpath"));
+        }
     }
 }
